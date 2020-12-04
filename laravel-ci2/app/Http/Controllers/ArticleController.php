b3163557d6ad342641403ea7669bc219876c34f3
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-12-04 14:54:51 +0900

    記事更新処理と記事削除処理の作成

--- laravel-ci2/app/Http/Controllers/ArticleController.php
+++ laravel-ci2/app/Http/Controllers/ArticleController.php
@@ -30,7 +30,20 @@ class ArticleController extends Controller
 
     public function edit(Article $article)
     {
-        // dd($article);
         return view('articles.edit', ['article' => $article]);
+    }
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+
+        return redirect()->route('articles.index');
+    }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+
+        return redirect()->route('articles.index');
     }
 }
