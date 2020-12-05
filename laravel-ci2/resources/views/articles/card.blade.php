d59f9c60bfcbdedeb06ff815de060b6119c998da
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-12-05 12:39:34 +0900

    card.blade.phpの間違え部分を修正 Vueコンポーネントからいいね・いいね解除を行う

--- laravel-ci2/resources/views/articles/card.blade.php
+++ laravel-ci2/resources/views/articles/card.blade.php
@@ -74,7 +74,7 @@
         :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'
         :initial-count-likes='@json($article->count_likes)'
         :authorized='@json(Auth::check())'
-        endpoint="{{ route('articles.like', ['article' => $article] }}"
+        endpoint="{{ route('articles.like', ['article' => $article]) }}"
       >
       </article-like>
     </div>
