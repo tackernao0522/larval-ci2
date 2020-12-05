0852f7d02257626f00f29c374ffda29dcbb697be
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-12-05 19:21:15 +0900

    記事更新処理でタグの登録と記事・タグの紐付け登録・削除を行う

--- laravel-ci2/app/Http/Controllers/ArticleController.php
+++ laravel-ci2/app/Http/Controllers/ArticleController.php
@@ -56,6 +56,12 @@ class ArticleController extends Controller
     {
         $article->fill($request->all())->save();
 
+        $article->tags()->detach();
+        $request->tags->each(function ($tagName) use ($article) {
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            $article->tags()->attach($tag);
+        });
+
         return redirect()->route('articles.index');
     }
 
