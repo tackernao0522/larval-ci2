7e5148d1b81670816b696247619a5a54dfe99430
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-12-06 15:35:53 +0900

    show.blade.phpでtabs.blade.phpを使用する

--- laravel-ci2/resources/views/users/show.blade.php
+++ laravel-ci2/resources/views/users/show.blade.php
@@ -4,20 +4,9 @@
 @include('nav')
 <div class="container">
   @include('users.user')
-    <ul class="nav nav-tabs nav-justified mt-3">
-      <li class="nav-item">
-        <a class="nav-link text-muted active" href="{{ route('users.show', [ 'name' => $user->name ]) }}">
-          記事
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-muted" href="{{ route('users.likes', ['name' => $user->name]) }}">
-          いいね
-        </a>
-      </li>
-    </ul>
-      @foreach ($articles as $article)
-        @include('articles.card')
-      @endforeach
+  @include('users.tabs', ['hasArticles' => true, 'hasLikes' => false])
+  @foreach ($articles as $article)
+  @include('articles.card')
+  @endforeach
 </div>
 @endsection
