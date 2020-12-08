607abf8fe81ec3d0d586ab1d74de6a1cebb6f467
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-12-08 21:00:36 +0900

    CircleCIのdeployジョブの移行-修正6

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,6 @@
 
 set -eux
 
-cd ~/laravel-ci
-cd laravel-ci2
-php artisan migrate --force
-php artisan config:cache
+cd ~/laravel-ci/laravel-ci2
+cd laravel-ci2/; php artisan migrate --force
+cd laravel-ci2/; php artisan config:cache
