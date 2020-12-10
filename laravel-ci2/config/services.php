cc3f9ccc29a6683f961f9572a08a25c2f7c82962
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-12-10 10:09:42 +0900

    config/services.phpの修正

--- laravel-ci2/config/services.php
+++ laravel-ci2/config/services.php
@@ -33,8 +33,8 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect'=>env('GOOGLE_CALLBACK_URL'),
-        // 'redirect' => env('APP_URL') . '/login/google/callback',
+        'redirect' => env('APP_URL') . '/login/google/callback',
+        // 'redirect'=>env('GOOGLE_CALLBACK_URL'),
     ],
 
 ];
